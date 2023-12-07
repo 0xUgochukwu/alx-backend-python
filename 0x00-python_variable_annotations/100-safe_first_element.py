dbd059481c279bd428fe60e7e8d8a528e3d8c43e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-07 16:19:23 +0100

    safe

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,10 +4,11 @@
     Variable Annotations
 '''
 
-from typing import Any, Sequence, Union 
+from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    ''' Return first Element '''
     if lst:
         return lst[0]
     else:
